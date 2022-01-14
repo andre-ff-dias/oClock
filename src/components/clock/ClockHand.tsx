8ebca79b80a27f40b8c17113d1622b852a342af3
andre_ff_dias <2160829@my.ipleiria.pt>
2022-01-14 23:44:23 +0000

    Create ClockHand.tsx

---
+++ src/components/clock/ClockHand.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ClockHand = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ClockHand;
