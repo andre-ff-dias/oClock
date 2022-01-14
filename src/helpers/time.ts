aacfb073baf28f86a070dfa964682deacd561677
andre_ff_dias <2160829@my.ipleiria.pt>
2022-01-14 23:48:22 +0000

    New functions - getTime && to12hClock

---
+++ src/helpers/time.ts
@@ -0,0 +1,17 @@
+export const to12hClock = (hour: number): number => {
+    return hour > 12 ? hour - 12 : hour;
+};
+
+type TimeObject = {
+    hours: number;
+    minutes: number;
+    seconds: number;
+};
+
+export const getTime = (): TimeObject => {
+    const date = new Date();
+    const hours = (to12hClock(date.getHours()) / 12) * 360;
+    const minutes = (date.getMinutes() / 60) * 360;
+    const seconds = (date.getSeconds() / 60) * 360;
+    return { hours, minutes, seconds };
+};
