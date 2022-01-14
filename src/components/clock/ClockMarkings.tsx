384a4dc159b191ae0c923e6757af0aa01ca772ac
andre_ff_dias <2160829@my.ipleiria.pt>
2022-01-14 23:38:46 +0000

    Create ClockMarkings.tsx

---
+++ src/components/clock/ClockMarkings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ClockMarkings = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ClockMarkings;
