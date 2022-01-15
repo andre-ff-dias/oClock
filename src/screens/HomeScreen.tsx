719176d9b39d8bd0d462cfddb894b6f327fa1508
andre_ff_dias <2160829@my.ipleiria.pt>
2022-01-15 13:05:09 +0000

    Rename file Home.tsx to HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const Home = () => {
+const HomeScreen = () => {
   return (
     <View>
-      <Text></Text>
+      <Text>Home</Text>
     </View>
   );
 };
 
-export default Home;
+export default HomeScreen;
