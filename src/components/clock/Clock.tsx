913c0dbe2a4105a59697b7b7a4f76fe48eacb216
andre_ff_dias <2160829@my.ipleiria.pt>
2022-01-15 14:30:31 +0000

    Update Clock.tsx

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Svg from 'react-native-svg';
 import {Dimensions} from 'react-native';
+import ClockMarkings from './ClockMarkings';
+import ClockHand from './ClockHand';
+import {getTime} from '../../helpers/time';
+import {useInterval} from '../../helpers/hooks';
 
 const {width} = Dimensions.get('window');
 const diameter = width - 40;
@@ -10,7 +14,43 @@ const hourStickCount = 12;
 const minuteStickCount = 12 * 6;
 
 const Clock = () => {
-  return <Svg height={width} width={width}></Svg>;
+  const [time, setTime] = useState(getTime);
+
+  useInterval(() => {
+    setTime(getTime);
+  }, 1000);
+
+  return (
+    <Svg height={width} width={width}>
+      <ClockMarkings
+        minutes={minuteStickCount}
+        hours={hourStickCount}
+        radius={radius}
+        center={center}
+      />
+      <ClockHand
+        angle={time.seconds}
+        center={center}
+        radius={radius}
+        stroke="red"
+        strokeWidth="1"
+      />
+      <ClockHand
+        angle={time.minutes}
+        center={center}
+        radius={radius}
+        stroke="white"
+        strokeWidth="5"
+      />
+      <ClockHand
+        angle={time.hours}
+        center={center}
+        radius={radius}
+        stroke="white"
+        strokeWidth="7"
+      />
+    </Svg>
+  );
 };
 
 export default Clock;
