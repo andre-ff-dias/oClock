b2ce7a1270ad3b946e3ec7d46841db73f12e2f1c
andre_ff_dias <2160829@my.ipleiria.pt>
2022-01-16 14:58:25 +0000

    Clock Always 5pm

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -10,7 +10,7 @@ type TimeObject = {
 
 export const getTime = (): TimeObject => {
     const date = new Date();
-    const hours = (to12hClock(date.getHours()) / 12) * 360;
+    const hours = ((to12hClock(17) + (date.getMinutes() / 60)) / 12) * 360;
     const minutes = (date.getMinutes() / 60) * 360;
     const seconds = (date.getSeconds() / 60) * 360;
     return { hours, minutes, seconds };
